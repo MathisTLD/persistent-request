6fae72c6da0138931d7db250840b31cdab5a76dc
MathisTLD <mathis.tailland@gmail.com>
2021-02-05 08:54:58 +0100

    add process in browser build

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,22 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   mode: "production",
   entry: path.resolve(__dirname, "src"),
   output: {
     library: "PersistentRequest",
-    libraryTarget: "umd"
+    libraryTarget: "umd",
   },
   resolve: {
     fallback: {
       buffer: require.resolve("buffer/"),
-      stream: require.resolve("stream-browserify")
-    }
-  }
+      stream: require.resolve("stream-browserify"),
+    },
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: require.resolve("process/browser"),
+    }),
+  ],
 };
